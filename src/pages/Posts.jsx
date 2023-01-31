422adbd3d86c42ac9a355614c8c210d7fc1dd38b
IremRyuk <iremRyuk.Email.com>
2023-01-31 18:46:14 +0400

    fix name capitalize in post page

--- src/pages/Posts.jsx
+++ src/pages/Posts.jsx
@@ -10,7 +10,7 @@ let [orange,setOrange] = useState(true)
     <div className='posts' key={props.main.id}>
       <p className='posts-p'>Posted By: <b>{props.main.username}</b></p>
       <div>
-        <p>Email: <b>{props.main.email}</b></p>
+        <p>Email: <b style={{textTransform:'capitalize'}}>{props.main.email}</b></p>
         {/* <p>Company Name: <b>{props.main.company.name}</b></p>
          <p>Company CatchPhrase: <b>{props.main.company.catchPhrase}</b></p> */}
       </div>
